5846f53f35f306b2d191a9a99cf220a92ee7455d
olaniSiyum <olisiyum@gmail.com>
2023-12-21 18:07:44 +0300

    added files

--- airflow/dags/script/extract_data_source.py
+++ airflow/dags/script/extract_data_source.py
@@ -1,31 +1,57 @@
+import sys
+from typing_extensions import Self
+
+from script.data_reader import DataReader
+from script.postgress_manager import create_tables, insert_data
+
+from datetime import datetime, timedelta
 import pandas as pd
+import os
 
-def get_data(**kwargs):
-    # Define the path to your CSV file
-    csv_path = '/home/olani/Downloads/20181024_d1_0830_0900.csv'
+dag_path = os.getcwd()
 
-    # Read the CSV file using pandas
-    try:
-        # Read the CSV file into a pandas DataFrame
-        df = pd.read_csv(csv_path,header=None, names=['data'])
+TRAJECTORY_SCHEMA = os.path.join(
+    dag_path, "dags/script/trajectory_info_schema.sql")
+VEHICLE_SCHEMA = os.path.join(dag_path, "dags/script/vehicle_info_schema.sql")
+
+class ELT:
+    def __init__(self, read_dag_path,save_dag_path):
+        self.read_dag_path = read_dag_path
+        self.save_dag_path = save_dag_path
+
+    def extract_data(self,file_path=None,) -> None:
+        if file_path is not None:
+            self.read_dag_path = file_path
+            
+        reader = DataReader()
+        vehicle_df, trajectories_df = reader.get_dfs(
+            file_path=self.read_dag_path, v=0)
         
-        # You can perform additional data preprocessing steps here if needed
-         # Split the single column into multiple columns based on semicolon separator
-        df = df['data'].str.split(';', expand=True)
+        vehicle_df.to_csv(os.path.join(self.save_dag_path,'vehicle_info.csv'),index=False)
+        trajectories_df.to_csv(os.path.join(self.save_dag_path,'trajectories_info.csv'), index=False)
+
+    def load_trajectory_data(self):
+        trajectories_df = pd.read_csv(os.path.join(self.save_dag_path, 'trajectories_info.csv'))
+        create_tables(TRAJECTORY_SCHEMA)
+        print('create trajectory table')
+        insert_data(trajectories_df[:10000], "trajectories")
+    
+    def load_vehicle_data(self):
+        vehicle_df = pd.read_csv(os.path.join(
+            self.save_dag_path, 'vehicle_info.csv'))
+        create_tables(VEHICLE_SCHEMA)
+        print('create vehicle table')
+        insert_data(vehicle_df, "vehicles")
         
-        # Optional: Assign meaningful column names to the new columns
-        #df.columns = ['Attribute_1', 'Attribute_2', 'Attribute_3', 'Attribute_4', 'Attribute_5',
-         #             'Attribute_6', 'Attribute_7', 'Attribute_8', 'Attribute_9', 'Attribute_10']
-        
-        # Perform any additional preprocessing steps here
-        
-        # Return the preprocessed DataFrame
-        return df
-        
-        # Return the DataFrame
-        return df
-        
-    except Exception as e:
-        # Handle exceptions if any
-        print(f"An error occurred while reading the CSV file: {e}")
-        return None
+    def execution_date_to_millis(self, execution_date):
+        """converts execution date (in DAG timezone) to epoch millis
+
+        Args:
+            date (execution date): %Y-%m-%d
+
+        Returns:
+            milliseconds
+        """
+        date = datetime.strptime(execution_date, "%Y-%m-%d")
+        epoch = datetime.utcfromtimestamp(0)
+        return (date - epoch).total_seconds() * 1000.0
---
+++ airflow/dags/script/extract_load.py
@@ -0,0 +1,57 @@
+import sys
+from typing_extensions import Self
+
+from script.data_reader import DataReader
+from script.postgress_manager import create_tables, insert_data
+
+from datetime import datetime, timedelta
+import pandas as pd
+import os
+
+dag_path = os.getcwd()
+
+TRAJECTORY_SCHEMA = os.path.join(
+    dag_path, "dags/script/trajectory_info_schema.sql")
+VEHICLE_SCHEMA = os.path.join(dag_path, "dags/script/vehicle_info_schema.sql")
+
+class ELT:
+    def __init__(self, read_dag_path,save_dag_path):
+        self.read_dag_path = read_dag_path
+        self.save_dag_path = save_dag_path
+
+    def extract_data(self,file_path=None,) -> None:
+        if file_path is not None:
+            self.read_dag_path = file_path
+            
+        reader = DataReader()
+        vehicle_df, trajectories_df = reader.get_dfs(
+            file_path=self.read_dag_path, v=0)
+        
+        vehicle_df.to_csv(os.path.join(self.save_dag_path,'vehicle_info.csv'),index=False)
+        trajectories_df.to_csv(os.path.join(self.save_dag_path,'trajectories_info.csv'), index=False)
+
+    def load_trajectory_data(self):
+        trajectories_df = pd.read_csv(os.path.join(self.save_dag_path, 'trajectories_info.csv'))
+        create_tables(TRAJECTORY_SCHEMA)
+        print('create trajectory table')
+        insert_data(trajectories_df[:10000], "trajectories")
+    
+    def load_vehicle_data(self):
+        vehicle_df = pd.read_csv(os.path.join(
+            self.save_dag_path, 'vehicle_info.csv'))
+        create_tables(VEHICLE_SCHEMA)
+        print('create vehicle table')
+        insert_data(vehicle_df, "vehicles")
+        
+    def execution_date_to_millis(self, execution_date):
+        """converts execution date (in DAG timezone) to epoch millis
+
+        Args:
+            date (execution date): %Y-%m-%d
+
+        Returns:
+            milliseconds
+        """
+        date = datetime.strptime(execution_date, "%Y-%m-%d")
+        epoch = datetime.utcfromtimestamp(0)
+        return (date - epoch).total_seconds() * 1000.0
